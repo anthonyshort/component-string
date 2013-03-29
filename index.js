4a21c918c7fa2db3f335de534335d02b1fca1623
Anthony Short <antshort+github@gmail.com>
2013-03-30 01:09:14 +1100

    Working

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var strtojs = require('string-to-js');
-var path = require('path');
-var fs = require('fs');
+var strtojs = require('string-to-js'),
+    path = require('path'),
+    resolve = path.resolve,
+    fs = require('fs');
 
 module.exports = function(builder) {
-  builder.hook('before scripts', function(pkg){
-    if( !pkg.conf.templates ) return pkg;
-    pkg.conf.templates.forEach(function(file){
-      if (path.extname(file) !== '.html') return;
-      var str = fs.readFileSync(file, 'utf8');
-      var compiled = strtojs(str);
-      pkg.addFile('scripts', file.replace('.html','.js'), compiled);
+  builder.on('config', function(){
+    (builder.conf.strings || []).forEach(function(file){
+      var str = fs.readFileSync(resolve(file), 'utf8');
+      builder.addFile('scripts', file, strtojs(str));
     });
-    return pkg;
   });
 };
