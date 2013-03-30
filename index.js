93f6b39740c6adbb8fc37ad0cb33341e55a5b718
Anthony Short <antshort+github@gmail.com>
2013-03-30 16:18:15 +1100

    Using the before scripts hook instead of the config hook. Fixes bugs with nested components

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var strtojs = require('string-to-js'),
     path = require('path'),
-    resolve = path.resolve,
     fs = require('fs');
 
 module.exports = function(builder) {
-  builder.on('config', function(){
-    (builder.conf.strings || []).forEach(function(file){
-      var str = fs.readFileSync(resolve(file), 'utf8');
-      builder.addFile('scripts', file, strtojs(str));
+  builder.hook('before scripts', function(pkg, next) {
+    var strings = pkg.conf.strings;
+    if( !strings || strings.length === 0 ) return next();
+
+    strings.forEach(function(file, i){
+      var str = fs.readFileSync(pkg.path(file), 'utf8');
+      pkg.addFile('scripts', file, strtojs(str));
     });
+
+    next();
   });
+
 };
